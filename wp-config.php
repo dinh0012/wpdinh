532bc86aa6cd1f01c6adfdbe37dba3e87547d77c
dinh0012 <dinh020304@gmail.com>
2017-02-10 20:33:42 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'u629339859_wp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u629339859_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '8DbVcJKOsLlm');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
