5736cd9bb5c208377cf0170b8c26fbd8a3b8824a
quynhtran <quynh.tranf@gmail.com>
2017-02-11 15:54:02 +0700

    fix code

--- wp-content/themes/twentyseventeen-child/archive.php
+++ wp-content/themes/twentyseventeen-child/archive.php
@@ -11,67 +11,66 @@
  */
 
 get_header(); ?>
+
 	<div class="wrap">
+
+		<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+		<?php endif; ?>
+
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 
 				<?php
-				/*while ( have_posts() ) : the_post();
+				if ( have_posts() ) : ?>
+					<?php
+					/* Start the Loop */
+					$count = 0;
+					while ( have_posts() ) : the_post();$count++;
 
-                    get_template_part( 'template-parts/page/content', 'page' );
-
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
-
-                endwhile; // End of the loop.*/
-
-				$temp = $wp_query;
-				$wp_query = null;
-				$wp_query = new WP_Query();
-				$wp_query->query('showposts=6&post_type=sanpham'.'&paged='.$paged);
-				$count = 0;
-
-				while ($wp_query->have_posts()) : $wp_query->the_post(); $count++;
-					?>
-
-					<?php //Tạo biến chèn class 'thrid' vào mỗi 3 bài viết.
 					if ($count == 4) {
 						$p3 = 'thrid';
 						$count = 0;
 					} else { $p3 = ''; }
 					?>
 
-					<div <?php post_class($p3); ?> id="product-<?php the_ID(); ?>">
-						<?php the_post_thumbnail('medium',array('class' => 'product-thumb') ); ?>
-						<a href="<?php the_permalink(); ?>"><h3 class="title-sp"><?php the_title(); ?></h3></a>
-						<div class="info">
-							<p style="margin-top: 5px;">Giá: <?php echo get_post_meta( $post->ID, 'price', true ); ?></p>
-							<p class="product-status">
-								<?php
-								$product_status = get_post_meta( $post->ID, 'author', true );
+				<div <?php post_class($p3); ?> id="product-<?php the_ID(); ?>">
+					<?php the_post_thumbnail('medium',array('class' => 'product-thumb') ); ?>
+					<a href="<?php the_permalink(); ?>"><h3 class="title-sp"><?php the_title(); ?></h3></a>
+					<div class="info">
+						<p style="margin-top: 5px;">Giá: <?php echo get_post_meta( $post->ID, 'price', true ); ?></p>
+						<p class="product-status">
+							<?php
+							$product_status = get_post_meta( $post->ID, 'author', true );
 
-								?>
-							</p><!--Tình trạng sản phẩm-->
-							<a class="order-button" href="<?php the_permalink();?>">Xem chi tiết</a>
-						</div>
+							?>
+						</p><!--Tình trạng sản phẩm-->
+						<a class="order-button" href="<?php the_permalink();?>">Xem chi tiết</a>
 					</div>
+				</div>
 
-				<?php endwhile; ?>
-<!--
-				<nav>
-					<?php /*previous_posts_link('<< Previous') */?>
-					<?php /*next_posts_link('Next >>') */?>
-				</nav>-->
+					<?php endwhile;
 
-				<?php
-				$wp_query = null;
-				$wp_query = $temp;  // Reset
-				?>
+					the_posts_pagination( array(
+						'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+						'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+						'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+					) );
+
+				else :
+
+					get_template_part( 'template-parts/post/content', 'none' );
+
+				endif; ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
+		<?php get_sidebar(); ?>
 	</div><!-- .wrap -->
 	<style>
 		.title-sp{
@@ -115,5 +114,4 @@ get_header(); ?>
 		}
 
 	</style>
-
 <?php get_footer();
--- wp-content/themes/twentyseventeen/shop.php
+++ wp-content/themes/twentyseventeen/shop.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying archive pages
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -14,108 +9,111 @@
  * @since 1.0
  * @version 1.0
  */
-/*
-Template Name: Shop
-*/
 
-/*-----CHÚNG TA SẼ VIẾT CODE PHÍA DƯỚI NÀY-----*/
 get_header(); ?>
+	<div class="wrap">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+				<?php
+				/*while ( have_posts() ) : the_post();
 
-			<?php
-			/*while ( have_posts() ) : the_post();
+                    get_template_part( 'template-parts/page/content', 'page' );
 
-				get_template_part( 'template-parts/page/content', 'page' );
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+                endwhile; // End of the loop.*/
 
-			endwhile; // End of the loop.*/
+				$temp = $wp_query;
+				$wp_query = null;
+				$wp_query = new WP_Query();
+				$wp_query->query('showposts=6&post_type=sanpham'.'&paged='.$paged);
+				$count = 0;
 
-			$temp = $wp_query;
-			$wp_query = null;
-			$wp_query = new WP_Query();
-			$wp_query->query('showposts=4&post_type=san_pham'.'&paged='.$paged);
-			$count = 0;
+				while ($wp_query->have_posts()) : $wp_query->the_post(); $count++;
+					?>
 
-			while ($wp_query->have_posts()) : $wp_query->the_post(); $count++;
-				?>
+					<?php //Tạo biến chèn class 'thrid' vào mỗi 3 bài viết.
+					if ($count == 4) {
+						$p3 = 'thrid';
+						$count = 0;
+					} else { $p3 = ''; }
+					?>
 
-				<?php //Tạo biến chèn class 'thrid' vào mỗi 3 bài viết.
-				if ($count == 4) {
-					$p3 = 'thrid';
-					$count = 0;
-				} else { $p3 = ''; }
-				?>
+					<div <?php post_class($p3); ?> id="product-<?php the_ID(); ?>">
+						<?php the_post_thumbnail('medium',array('class' => 'product-thumb') ); ?>
+						<a href="<?php the_permalink(); ?>"><h3 class="title-sp"><?php the_title(); ?></h3></a>
+						<div class="info">
+							<p style="margin-top: 5px;">Giá: <?php echo get_post_meta( $post->ID, 'price', true ); ?></p>
+							<p class="product-status">
+								<?php
+								$product_status = get_post_meta( $post->ID, 'author', true );
 
-				<div <?php post_class($p3); ?> id="product-<?php the_ID(); ?>">
-					<?php the_post_thumbnail('medium',array('class' => 'product-thumb') ); ?>
-					<a href="<?php the_permalink(); ?>"><h3 class="title"><?php the_title(); ?></h3></a>
-					<div class="info">
-						<p style="margin-top: 5px;">Giá: <?php echo get_post_meta( $post->ID, 'price', true ); ?></p>
-						<p class="product-status">
-							<?php
-							$product_status = get_post_meta( $post->ID, 'author', true );
-
-							?>
-						</p><!--Tình trạng sản phẩm-->
-						<a class="order-button" href="<?php the_permalink();?>">Xem chi tiết</a>
+								?>
+							</p><!--Tình trạng sản phẩm-->
+							<a class="order-button" href="<?php the_permalink();?>">Xem chi tiết</a>
+						</div>
 					</div>
-				</div>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
+				<!--
+				<nav>
+					<?php /*previous_posts_link('<< Previous') */?>
+					<?php /*next_posts_link('Next >>') */?>
+				</nav>-->
 
-			<nav>
-				<?php previous_posts_link('<< Previous') ?>
-				<?php next_posts_link('Next >>') ?>
-			</nav>
+				<?php
+				$wp_query = null;
+				$wp_query = $temp;  // Reset
+				?>
 
-			<?php
-			$wp_query = null;
-			$wp_query = $temp;  // Reset
-			?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .wrap -->
+	<style>
+		.title-sp{
+			font-size: 16px;
+			padding-top: 10px !important;
+		}
+		#content #primary{
+			width: 100%;
+		}
+		.product-thumb {
+			float: left;
+		}
+		.product-info {
+			width: 45%;
+			margin-left: 5%;
+			float: right;
+		}
+		.product-info h1 {
+			font-size: 1.5em;
+			margin-bottom: 0.5em;
+		}
+		.product-price, .product-status, .product-description {
+			margin: 5px 0;
+		}
+		.order-button {
+			color: #fff;
+			background: rgb(33, 189, 12);
+			text-decoration: none;
+			padding: 5px;
+			margin: 15px 0;
+			display: table;
+		}
+		.post-info {
+			clear: both;
+			padding-top: 15px;
+		}
+		.sanpham {
+			width: 30.3333%;
+			float: left;
+			margin-right: 3%;
+		}
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-<style>
-	.product-thumb {
-		float: left;
-	}
-	.product-info {
-		width: 45%;
-		margin-left: 5%;
-		float: right;
-	}
-	.product-info h1 {
-		font-size: 1.5em;
-		margin-bottom: 0.5em;
-	}
-	.product-price, .product-status, .product-description {
-		margin: 5px 0;
-	}
-	.order-button {
-		color: #fff;
-		background: rgb(33, 189, 12);
-		text-decoration: none;
-		padding: 5px;
-		margin: 15px 0;
-		display: table;
-	}
-	.post-info {
-		clear: both;
-		padding-top: 15px;
-	}
-	.san_pham {
-		width: 22%;
-		float: left;
-		margin-right: 3%;
-	}
+	</style>
 
-</style>
 <?php get_footer();
