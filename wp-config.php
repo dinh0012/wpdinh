fda15fe6b1292b8902a68b4cfea55a75b867c3fe
quynhtran <quynh.tranf@gmail.com>
2017-02-11 14:43:45 +0700

    config

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'u629339859_wp');
-ad
+
 /** MySQL database username */
 define('DB_USER', 'u629339859_root');
 
