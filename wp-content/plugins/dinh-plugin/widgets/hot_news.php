0f42998b22b96c6794fa6c0ceb04963dbea43606
quynhtran <quynh.tranf@gmail.com>
2017-02-15 16:24:12 +0700

    git púh

--- wp-content/plugins/dinh-plugin/widgets/hot_news.php
+++ wp-content/plugins/dinh-plugin/widgets/hot_news.php
@@ -36,11 +36,19 @@ class Hot_News extends WP_Widget
         $instance = $old_instance;
         $instance['title'] = strip_tags($new_instance['title']);
         $instance['num_post'] = strip_tags($new_instance['num_post']);
+        $instance['interval'] = strip_tags($new_instance['interval']);
+        $cat = implode(',', $new_instance['cat']);
+
+        $instance['cat'] = $cat;
         return $instance;
+
     }
 
     public function form($instance)
     {
+        $taxonomies = get_taxonomies(['name' => 'loai-san-pham'], 'object');
+        $loai_sp = get_terms($taxonomies['loai-san-pham']->name);
+        //var_dump($loai_sp);
         $inputID = $this->get_field_id('title');
         $inputName = $this->get_field_name('title');
         $inputValue = @$instance['title'];
@@ -57,18 +65,36 @@ class Hot_News extends WP_Widget
         $html = '';
         $html .= '<p>';
         $html .= '<label for=' . $inputID . '>Number Post :</label>';
-        $html .= '<input class="widefat" type="text" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
+        $html .= '<input class="widefat" type="number" step="1" min="1" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
+        $html .= '</p>';
+        echo $html;
+
+        $inputID = $this->get_field_id('interval');
+        $inputName = $this->get_field_name('interval');
+        $inputValue = @$instance['interval'];
+        $html = '';
+        $html .= '<p>';
+        $html .= '<label for=' . $inputID . '>Time to delay (sec) :</label>';
+        $html .= '<input class="widefat" type="number" step="1" min="1" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
         $html .= '</p>';
         echo $html;
 
         $inputID = $this->get_field_id('cat');
         $inputName = $this->get_field_name('cat');
-        $inputValue = @$instance['cat'];
+        $cat = @$instance['cat'];
+        $cat_arr = explode(',', $cat);
         $html = '';
         $html .= '<p>';
         $html .= '<label for=' . $inputID . '>Choose Categgory :</label>';
-        $html .= '<input class="widefat" type="checkbox" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
-        $html .= '<input class="widefat" type="checkbox" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
+        foreach ($loai_sp as $sp) {
+            $html .= '<br>';
+            $check = '';
+            if ($check_cat = in_array ($sp->term_id, $cat_arr))
+                $check = 'checked';
+            $html .= '<input class="checkbox" type="checkbox" ' . $check . ' name= "' . $inputName . '[]" value="' . $sp->term_id . '"/>';
+            $html .= "<label>" . $sp->name . "</label>";
+        }
+        //$html .= '<input class="widefat" type="hidden" name= "' . $inputName . '" id="' . $inputID . '" value = "' . $inputValue . '" size="25" />';
         $html .= '</p>';
         echo $html;
 
