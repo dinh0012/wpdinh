a8476afb8fed681ae6fcd9011a56bffb729278b6
quynhtran <quynh.tranf@gmail.com>
2017-02-15 16:09:29 +0700

    cmm

--- wp-content/plugins/dinh-plugin/dinh-plugin.php
+++ wp-content/plugins/dinh-plugin/dinh-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package Kenshin Widget
-* @version 0.1
-*/
+ * @version 0.1
+ */
 /*
 Plugin Name: Dinh Plugin
 Plugin URI: http://laptrinhweb.org
@@ -11,35 +11,42 @@ Author: dinh0012
 Version: 0.1
 Author URI: None
 */
- 
+
 define('WIDGET_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('WIDGET_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WIDGET_VIEWS_DIR', WIDGET_PLUGIN_DIR . '/views');
-define('WIDGET_INCLUDES_DIR', WIDGET_PLUGIN_DIR .'/includes');
+define('WIDGET_INCLUDES_DIR', WIDGET_PLUGIN_DIR . '/includes');
 define('WIDGET_DIR', WIDGET_PLUGIN_DIR . '/widgets');
-if(!is_admin()){
-  }
-
-require_once WIDGET_DIR. '/widget_demo.php';
-require_once WIDGET_DIR. '/members.php';
-require_once WIDGET_DIR. '/contact.php';
-add_action( 'widgets_init', 'create_widget' );
-function create_widget() {
-        register_widget('Widget_Demo');
-         register_widget('Members');
-         register_widget('Contact');
+if (!is_admin()) {
 }
 
-function create_youtube_shortcode( $args, $content ) {
-        $content = '<iframe src="//www.youtube.com/embed/'.$args['id'].'" height=" '.$args['height'].'" width="'.$args['width'].'" allowfullscreen="" frameborder="0"></iframe>';
- return $content;
+require_once WIDGET_DIR . '/widget_demo.php';
+require_once WIDGET_DIR . '/members.php';
+require_once WIDGET_DIR . '/contact.php';
+require_once WIDGET_DIR . '/hot_news.php';
+add_action('widgets_init', 'create_widget');
+function create_widget()
+{
+    register_widget('Widget_Demo');
+    register_widget('Members');
+    register_widget('Contact');
+    register_widget('Hot_News');
 }
+
+function create_youtube_shortcode($args, $content)
+{
+    $content = '<iframe src="//www.youtube.com/embed/' . $args['id'] . '" height=" ' . $args['height'] . '" width="' . $args['width'] . '" allowfullscreen="" frameborder="0"></iframe>';
+    return $content;
+}
+
 add_shortcode('youtube', 'create_youtube_shortcode');
 
-function create_banner_shortcode( $args, $content ) {
-        $content = '<iframe src="//www.youtube.com/embed/'.$args['id'].'" height=" '.$args['height'].'" width="'.$args['width'].'" allowfullscreen="" frameborder="0"></iframe>';
- return $content;
+function create_banner_shortcode($args, $content)
+{
+    $content = '<iframe src="//www.youtube.com/embed/' . $args['id'] . '" height=" ' . $args['height'] . '" width="' . $args['width'] . '" allowfullscreen="" frameborder="0"></iframe>';
+    return $content;
 }
+
 add_shortcode('youtube', 'create_youtube_shortcode');
 
 //******************************************************************
@@ -58,19 +65,21 @@ add_shortcode('youtube', 'create_youtube_shortcode');
         'example_dashboard_widget_function' // Display function.
     );
 }*/
-add_action( 'wp_dashboard_setup', 'example_add_dashboard_widgets' );
+add_action('wp_dashboard_setup', 'example_add_dashboard_widgets');
 
 /**
  * Create the function to output the contents of our Dashboard Widget.
  */
-function example_dashboard_widget_function() {
+function example_dashboard_widget_function()
+{
 
     // Display whatever it is you want to show.
     echo "Hello World, I'm a great Dashboard Widget";
 }
 
-function example_add_dashboard_widgets() {
-    wp_add_dashboard_widget( 'example_dashboard_widget', 'Example Dashboard Widget', 'example_dashboard_widget_function' );
+function example_add_dashboard_widgets()
+{
+    wp_add_dashboard_widget('example_dashboard_widget', 'Example Dashboard Widget', 'example_dashboard_widget_function');
 
     // Globalize the metaboxes array, this holds all the widgets for wp-admin
 
@@ -83,12 +92,12 @@ function example_add_dashboard_widgets() {
 
     // Backup and delete our new dashboard widget from the end of the array
 
-    $example_widget_backup = array( 'example_dashboard_widget' => $normal_dashboard['example_dashboard_widget'] );
-    unset( $normal_dashboard['example_dashboard_widget'] );
+    $example_widget_backup = array('example_dashboard_widget' => $normal_dashboard['example_dashboard_widget']);
+    unset($normal_dashboard['example_dashboard_widget']);
 
     // Merge the two arrays together so our widget is at the beginning
 
-    $sorted_dashboard = array_merge( $example_widget_backup, $normal_dashboard );
+    $sorted_dashboard = array_merge($example_widget_backup, $normal_dashboard);
 
     // Save the sorted array back into the original metaboxes
 
@@ -96,18 +105,20 @@ function example_add_dashboard_widgets() {
 }
 
 
-function remove_dashboard_meta() {
-    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+function remove_dashboard_meta()
+{
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+    remove_meta_box('dashboard_primary', 'dashboard', 'side');
+    remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+    remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal');//since 3.8
 }
-add_action( 'admin_init', 'remove_dashboard_meta' );
+
+add_action('admin_init', 'remove_dashboard_meta');
 add_option('name', 'dinh0012');
 //setting API
 
